ab51f00175271b8d585180cc5189da8318ebc20c
DanielHyeon <parkdod@gmail.com>
2026-01-01 01:31:44 +0900

    DataUtil.cs 리팩토링

--- SQLiteTestConsole/obj/Debug/net8.0/SQLiteTestConsole.AssemblyInfo.cs
+++ SQLiteTestConsole/obj/Debug/net8.0/SQLiteTestConsole.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SQLiteTestConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7813d17d797f8d8abb6c885ba9f40084b9028ffb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d67b3ed51de35145315efb9ecf69599d3860692")]
 [assembly: System.Reflection.AssemblyProductAttribute("SQLiteTestConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SQLiteTestConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
